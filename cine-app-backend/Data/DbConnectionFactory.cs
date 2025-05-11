232ea289750656d1d785c67b63b098249342e97e
Carlos <carloselima2001@gmail.com>
2025-05-10 21:54:34 -0300

    feat: Cria ConnectionString

---
+++ cine-app-backend/Data/DbConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace cine_app_backend.Data
+{
+    public class DbConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            return new SqlConnection(connectionString);
+        }
+    }
+}
